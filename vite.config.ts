738e94ec0c4ba30cceb44aa8f46e9e0d7a2189b7
XTL676 <tianluoqaq@qq.com>
2025-08-24 19:08:43 +0800

    build(vite): 优化生产环境配置并添加代码分析工具
    - 在 .gitignore 中添加 build目录
    - 在 package.json 中添加 rollup-plugin-visualizer依赖
    - 引入 visualizer插件进行代码打包分析
    - 优化生产环境的 sourcemap 配置
    - 设置生产环境变量 PROD_NAME
    - 调整输出目录为 ./build/dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,25 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const PROD_NAME = 'production'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode, command }) => ({
   define: {
-    __PROD__: JSON.stringify(mode === 'production'),
+    __PROD__: JSON.stringify(mode === PROD_NAME),
   },
 
   plugins: [
     vue(),
     // eslint-disable-next-line new-cap
     UnoCSS(),
+    visualizer({
+      open: false, // 构建后自动打开报告
+      gzipSize: true, // 显示 gzip 压缩大小
+      filename: './build/pack-stats.html' // 输出文件名
+    }),
   ],
 
   resolve: {
@@ -34,11 +42,11 @@ export default defineConfig(({ mode, command }) => ({
       },
     },
     // 开启 CSS 代码分割，开发调试用
-    devSourcemap: true,
+    devSourcemap: mode !== PROD_NAME,
   },
 
   build: {
-    sourcemap: false,
+    sourcemap: mode !== PROD_NAME,
     minify: 'esbuild',
     // 按 chunk 切割 css
     cssCodeSplit: true,
@@ -46,6 +54,7 @@ export default defineConfig(({ mode, command }) => ({
       // 生产环境不打入一些特定的包
       external: command === 'build' ? ['tslog'] : [],
     },
+    outDir: './build/dist',
   },
 
   esbuild: {
